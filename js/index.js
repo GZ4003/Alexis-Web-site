d6f2c679abe24ff12fe8dc04382f692bc653f7a7
GZ4003 <gz.alexis2@gmail.com>
2023-09-11 15:52:49 -0300

    upload cv

--- js/index.js
+++ js/index.js
@@ -47,7 +47,7 @@ ScrollReveal().reveal('.home-content, p, .about-content', { origin: "right" });
 
 
 const typed = new Typed(".multiple-text", {
-    strings: ["Frontend Developer", "Backend Developer", "Design UI"],
+    strings: ["DevOps Engineer", "Backend Developer", "Frontend Developer"],
     typeSpeed:100,
     backSpeed:100,
     backDelay:1000,
